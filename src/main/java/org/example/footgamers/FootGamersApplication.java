a64257aaba2dec5bf463dc878f685ffa65c86065
ElderJV <elderjulcavelasco@gmail.com>
2026-09-13 22:56:36 -0500

    primer commit

---
+++ src/main/java/org/example/footgamers/FootGamersApplication.java
@@ -0,0 +1,13 @@
+package org.example.footgamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootGamersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootGamersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/footgamers/FootGamersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.footgamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootGamersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
